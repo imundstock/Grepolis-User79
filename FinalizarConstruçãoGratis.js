c9e4c29cc9da80c7efd3eaffd088e99b6acd93f8
imundstock <161329885+imundstock@users.noreply.github.com>
2025-09-16 01:38:09 -0300

    Update FinalizarConstruçãoGratis.js

--- FinalizarConstruçãoGratis.js
+++ FinalizarConstruçãoGratis.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Finaliza construções automaticamente quando grátis (menos de 5 minutos)
-// @author       Você
+// @author       Hannzo
 // @match        *://*.grepolis.com/game/*
 // @grant        none
 // ==/UserScript==
