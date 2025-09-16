25d007849d2f0572a2116653b01a5158a84cca73
imundstock <161329885+imundstock@users.noreply.github.com>
2025-09-16 15:38:29 -0300

    Update AutoMultiStopCast.js

--- AutoMultiStopCast.js
+++ AutoMultiStopCast.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name         AutoMulti (Parar antes de aceitar CastPowerQuest)
-// @description  Para automaticamente quando a missão "Lançar um poder divino" for concluída, sem aceitá-la
-// @version      1.1.4
-// @author       Hannzo
-// @match        http://*br79.grepolis.com/game/*
-// @match        https://*br79.grepolis.com/game/*
-// ==/UserScript==
-
 (function() {
     'use strict';
 
